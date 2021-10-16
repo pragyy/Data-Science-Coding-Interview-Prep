ef99a25f0addcf50e87f4f6e16c795ed79428eca
Pragya Verma <pragyasurendra.verma@gmail.com>
2021-10-16 13:03:48 -0700

    Count Number of Pairs With Absolute Difference K

---
+++ LeetCode/Arrays/Count_Number_of_Pairs_With_Absolute_Difference_K.py
@@ -0,0 +1,25 @@
+"""
+
+Given an integer array nums and an integer k, return the number of pairs (i, j) where i < j such that |nums[i] - nums[j]| == k.
+
+The value of |x| is defined as:
+
+x if x >= 0.
+-x if x < 0.
+
+"""
+
+class Solution(object):
+    def countKDifference(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        add = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if abs(nums[i] - nums[j]) == k:
+                    add += 1
+                    
+        return add
