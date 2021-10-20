72cd33c498fac752dafc3b7b45fb84ca4863dfdc
Pragya Verma <pragyasurendra.verma@gmail.com>
2021-10-20 00:40:33 -0700

    greatest common divisor of array

---
+++ LeetCode/Arrays/Find Greatest Common_/Find_/Greatest_Common_Divisor_of_Array.py
@@ -0,0 +1,21 @@
+"""
+Given an integer array nums, return the greatest common divisor of the smallest number and largest number in nums.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+"""
+
+class Solution(object):
+    def findGCD(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        x = max(nums)
+        y = min(nums)
+        
+        while(y):
+            x, y = y, x % y
+  
+        return x
