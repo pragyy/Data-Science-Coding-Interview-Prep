b94ba35bedc006e67bf8f88d8ac836990c40bcee
Pragya Verma <pragyasurendra.verma@gmail.com>
2021-10-20 17:52:13 -0700

    Unique Morse Code Words

---
+++ LeetCode/Arrays/Unique_Morse_Code_Words.py
@@ -0,0 +1,32 @@
+"""
+International Morse Code defines a standard encoding where each letter is mapped to a series of dots and dashes, as follows:
+
+'a' maps to ".-",
+'b' maps to "-...",
+'c' maps to "-.-.", and so on.
+For convenience, the full table for the 26 letters of the English alphabet is given below:
+
+[".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+Given an array of strings words where each word can be written as a concatenation of the Morse code of each letter.
+
+For example, "cab" can be written as "-.-..--...", which is the concatenation of "-.-.", ".-", and "-...". We will call such a concatenation the transformation of a word.
+Return the number of different transformations among all words we have.
+"""
+
+class Solution(object):
+    def uniqueMorseRepresentations(self, words):
+        """
+        :type words: List[str]
+        :rtype: int
+        """
+        morse_code = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."]
+        
+        code = []
+        for word in words:
+            s = ''
+            for letter in word:
+                pos = ord(letter) - 96 - 1
+                s += morse_code[pos]
+            code.append(s)
+            
+        return len(set(code))
