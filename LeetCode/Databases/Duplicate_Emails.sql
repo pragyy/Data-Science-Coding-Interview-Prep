63a1bcac169ad9cbeac2fbe723af554e8f814bb4
Pragya Verma <pragyasurendra.verma@gmail.com>
2021-11-23 20:46:15 -0800

    Duplicate Emails

---
+++ LeetCode/Databases/Duplicate_Emails.sql
@@ -0,0 +1,22 @@
+/*
+able: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write an SQL query to report all the duplicate emails.
+
+Return the result table in any order.
+*/
+
+# Write your MySQL query statement below
+SELECT email as Email
+FROM Person 
+HAVING COUNT(email)>1
