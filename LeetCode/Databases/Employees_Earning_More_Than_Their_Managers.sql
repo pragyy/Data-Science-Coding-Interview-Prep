f5bd3111f8c1f6e0e4fb672dde71b4e63499195c
Pragya Verma <pragyasurendra.verma@gmail.com>
2021-11-23 20:43:14 -0800

    Employees Earning More Than Their Managers

---
+++ LeetCode/Databases/Employees_Earning_More_Than_Their_Managers.sql
@@ -0,0 +1,26 @@
+/*
+Table: Employee
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
+| salary      | int     |
+| managerId   | int     |
++-------------+---------+
+id is the primary key column for this table.
+Each row of this table indicates the ID of an employee, their name, salary, and the ID of their manager.
+ 
+
+Write an SQL query to find the employees who earn more than their managers.
+
+Return the result table in any order.
+*/
+
+# Write your MySQL query statement below
+SELECT T2.name As Employee
+FROM Employee T1 
+Inner join Employee T2
+ON T1.id = T2.managerId
+WHERE T1.salary < T2.salary
