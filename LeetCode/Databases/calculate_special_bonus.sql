2532d674238247a3f9d733617651f72f64610598
Pragya Verma <pragyasurendra.verma@gmail.com>
2022-06-14 11:44:20 -0700

    Create calculate_special_bonus.sql

---
+++ LeetCode/Databases/calculate_special_bonus.sql
@@ -0,0 +1,27 @@
+\*
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| employee_id | int     |
+| name        | varchar |
+| salary      | int     |
++-------------+---------+
+employee_id is the primary key for this table.
+Each row of this table indicates the employee ID, employee name, and salary.
+ 
+
+Write an SQL query to calculate the bonus of each employee. The bonus of an employee is 100% of their salary if the ID of the employee is an odd number and the employee name does not start with the character 'M'. The bonus of an employee is 0 otherwise.
+
+Return the result table ordered by employee_id.
+
+*\
+
+# Write your MySQL query statement below
+SELECT employee_id, 
+CASE
+    WHEN employee_id % 2 != 0 AND name NOT LIKE 'M%' THEN salary
+    ELSE 0
+END as bonus
+FROM Employees
+ORDER BY employee_id
