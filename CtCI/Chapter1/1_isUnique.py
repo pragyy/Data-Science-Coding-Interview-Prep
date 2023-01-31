183d8e9c6717f863145c80a285e8ffa632a37a4b
Pragya Verma <pragyasurendra.verma@gmail.com>
2023-01-30 16:14:35 -0800

    housing data

--- CtCI/Chapter1/1_isUnique.py
+++ CtCI/Chapter1/1_isUnique.py
@@ -1,1 +1,34 @@
+import unittest
 
+def unique(string):
+    
+    if len(string) > 256:
+        return False
+
+    char_list = []
+
+    for char in string:
+        if char not in char_list:
+            char_list.append(char)
+        
+        if char in char_list:
+            return False
+
+    return True
+
+class Test(unittest.TestCase):
+    dataT = [('abcd'), ('s4fad'), ('')]
+    dataF = [('23ds2'), ('hb 627jh=j ()')]
+
+    def test_unique(self):
+        # true check
+        for test_string in self.dataT:
+            actual = unique(test_string)
+            self.assertTrue(actual)
+        # false check
+        for test_string in self.dataF:
+            actual = unique(test_string)
+            self.assertFalse(actual)
+
+if __name__ == "__main__":
+    unittest.main()
